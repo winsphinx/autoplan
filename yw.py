5d933a37e6c757a5c581908da4b4ed555ff19fb0
winsphinx <winsphinx@gmail.com>
2021-04-10 09:24:24 +0800

    support parameters

--- yw.py
+++ yw.py
@@ -3,8 +3,10 @@
 
 import base64
 import datetime
+import getopt
 import json
 import os
+import sys
 import time
 
 import colorama
@@ -26,6 +28,28 @@ def dec(s):
 
 def enc(s):
     return str(base64.b64encode(bytes(s, 'utf-8')), 'utf-8')
+
+
+def add_user(user, pswd):
+    data = read_data()
+    data[user] = enc(pswd)
+    write_data(data)
+
+
+def del_user(user):
+    data = read_data()
+    del data[user]
+    write_data(data)
+
+
+def read_data():
+    with open("password.json", 'r') as f:
+        return json.load(f)
+
+
+def write_data(data):
+    with open("password.json", 'w') as f:
+        json.dump(data, f)
 
 
 def get_file_list(file_dir):
@@ -77,20 +101,25 @@ def change_date():
         edit_file(file)
 
 
-def login():
+def login(username):
     browser = webdriver.Ie()
     browser.get("http://10.202.3.27/WebApp/emoss/index.jsp")
 
     n = browser.find_element_by_id("tfAccount")
-    old_name = n.get_attribute('value')
-    name = input(f"\n名字（回车还用 {old_name} ，或输入新的）: ")
 
-    if name == "":
-        name = old_name.lower()
+    if username:
+        name = username
     else:
-        name = name.lower()
-        n.clear()
-        n.send_keys(name)
+        old_name = n.get_attribute('value')
+        name = input(f"\n名字（回车还用 {old_name} ，或输入新的）: ")
+
+        if name == "":
+            name = old_name.lower()
+        else:
+            name = name.lower()
+
+    n.clear()
+    n.send_keys(name)
 
     with open("password.json", 'r') as f:
         password = json.load(f)
@@ -127,12 +156,10 @@ def do_tasklist(browser, tasks):
         ActionChains(browser).double_click(id).perform()
         time.sleep(3)
         windows = browser.window_handles
-        # print(windows)
         for w in windows:
             if w != main_windows:
                 browser.switch_to.window(w)
-                filename = os.path.join(os.getcwd(), "files",
-                                        (tasks[i] + ".xls"))
+                filename = os.path.join(os.getcwd(), "files", (tasks[i] + ".xls"))
                 try:
                     browser.find_element_by_id("FILE").send_keys(filename)
                     browser.find_element_by_id("save").click()
@@ -148,12 +175,12 @@ def do_tasklist(browser, tasks):
                     browser.switch_to.window(main_windows)
 
 
-if __name__ == '__main__':
+def main(username=""):
     print(f"{RED}修改文件日期。。。{END}")
     change_date()
     print(f"{GREEN}文件日期改好了。。。{END}")
 
-    browser = login()
+    browser = login(username)
 
     last_list = list()
 
@@ -168,3 +195,35 @@ if __name__ == '__main__':
             time.sleep(5)
 
     print("\n\n完成！")
+
+
+if __name__ == '__main__':
+    tip = """
+    参数:
+            yw -a username password -- 增加用户名和密码
+            yw -d username          -- 删除指定的用户名
+            yw -u username          -- 以指定用户名登录
+    """
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "-h-a:-d:-u:")
+
+        if len(opts) == 0 and len(args) == 0:
+            main()
+        else:
+            for k, v in opts:
+                if k == "-a":
+                    if len(args) == 1:
+                        add_user(v, args[0])
+                    elif len(args) < 1:
+                        print("缺少密码")
+                    else:
+                        print("参数太多")
+                elif k == "-d":
+                    del_user(v)
+                elif k == "-h":
+                    print(tip)
+                elif k == "-u":
+                    main(v)
+    except getopt.GetoptError:
+        print(tip)
